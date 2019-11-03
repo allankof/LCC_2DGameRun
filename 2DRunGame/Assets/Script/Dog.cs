fff704100d203361f73439f1143a7cd959e2ba2b
allankof <54761510+allankof@users.noreply.github.com>
2019-11-03 12:14:49 +0800

    整理腳本

--- 2DRunGame/Assets/Script/Dog.cs
+++ 2DRunGame/Assets/Script/Dog.cs
@@ -6,9 +6,9 @@ public class Dog : MonoBehaviour
     [Header("跳躍次數")]      //標題
     [Range(1, 2)]             //範圍
     public int jumpCount = 2;     //欄位 field (屬性)
-    [Header("跳躍高度"), Range(1, 20)]
-    public int jumpHigh = 5;
-    [Header("速度"), Range(1.0f, 30.0f)]
+    [Header("跳躍高度"), Range(0, 2000)]
+    public int jumpHigh = 300;
+    [Header("速度"), Range(0, 30.0f)]
     public float speed = 10.5f;
     [Header("滑行距離"), Range(1.0f, 10.0f)]
     public float slide = 5.0f;
@@ -17,13 +17,24 @@ public class Dog : MonoBehaviour
     [Header("名稱")]
     public string dogName = "Jaga";
     //變型欄位
-    public Transform man, cam;
+    private Transform cam;
+    // 動畫控制器元件
+    private Animator ani;
+    // 2D膠囊碰撞器元件
+    private CapsuleCollider2D cc2d;
+    // 剛體元件
+    private Rigidbody2D r2d;
     #endregion
 
     // 起始事件: 開始時執行一次
     private void Start()
     {
-        
+        // 取得攝影機物件
+        cam = GameObject.Find("Main Camera").transform;
+        // 取得元件, GetComponent<T>() 泛型
+        ani = GetComponent<Animator>();
+        cc2d = GetComponent<CapsuleCollider2D>();
+        r2d = GetComponent<Rigidbody2D>();
     }
     // 更新事件: 每一禎執行一次 unity預設為60fps
     private void Update()
@@ -32,13 +43,25 @@ public class Dog : MonoBehaviour
         MoveCamera();
         
     }
+    /// <summary>
+    /// 碰撞地板監聽
+    /// </summary>
+    /// <param name="collision"></param>
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "地板")
+        {
+            isGround = true;
+            Debug.Log(collision.gameObject.name);
+        }
+    }
 
     /// <summary>
     /// 角色移動方法
     /// </summary>
     private void MoveMan() {
         // Time.deltaTime為裝置每一禎的時間,如60fps則為1/60秒
-        man.Translate(speed*Time.deltaTime, 0, 0);
+        transform.Translate(speed*Time.deltaTime, 0, 0);
     }
 
     /// <summary>
@@ -52,13 +75,39 @@ public class Dog : MonoBehaviour
     /// 角色跳躍方法
     /// </summary>
     public void DoJump() {
-        man.Translate(0, jumpHigh*Time.deltaTime, 0);
+        if (isGround == true)
+        {
+            ani.SetBool("跳躍開關", true);
+            r2d.AddForce(new Vector2(0, jumpHigh));
+            isGround = false;
+        }
     }
 
     /// <summary>
-    /// 角色滑行方法
+    /// 角色滑行方法,設定滑行時collider大小
     /// </summary>
     public void DoSlide() {
-        man.Translate(slide*Time.deltaTime, 0 ,0);
+        transform.Translate(slide*Time.deltaTime, 0 ,0);
+        ani.SetBool("滑行開關", true);
+
+        cc2d.offset = new Vector2(-0.52f, -0.85f);
+        cc2d.size = new Vector2(2.2f, 2.2f);
+        
+        //cc2d.offset.Set(-0.52f, -0.85f);
+        //cc2d.size.Set(2.2f, 2.2f);
+    }
+    /// <summary>
+    /// 重置角色跳躍與滑行,重置collider大小
+    /// </summary>
+    public void ResetAnimator()
+    {
+        ani.SetBool("跳躍開關", false);
+        ani.SetBool("滑行開關", false);
+
+        cc2d.offset = new Vector2(-0.52f, 0.15f);
+        cc2d.size = new Vector2(2.2f, 4.2f);
+
+        //cc2d.offset.Set(-0.52f, 0.15f);
+        //cc2d.size.Set(2.2f, 4.2f);
     }
 }
